40dd35e2490fa09ecf7adbd0b707555ae8e66bf0
Duvauwn Prendergast <duvauwnprendergast@gmail.com>
2021-10-23 07:10:42 +0100

    changing gameboard creation and adding dynamics

--- script.js
+++ script.js
@@ -2,38 +2,60 @@
 const Gameboard = () => {
     let board = [
         //Values for each part of the board currently manually input for setup
-        { 0: 'X' },
-        { 1: 'O' },
-        { 2: 'X' },
-        { 3: 'X' },
-        { 4: 'O' },
-        { 5: 'O' },
-        { 6: 'O' },
-        { 7: 'O' },
-        { 8: 'X' },
+        { 0: null },
+        { 1: null },
+        { 2: null },
+        { 3: null },
+        { 4: null },
+        { 5: null },
+        { 6: null },
+        { 7: null },
+        { 8: null },
     ];
     return { board };
 }
 
 //The players will also be stored inside objects
 const Player = () => {
+    let u = 0;
+    let turn = () => {
+        u++
+        let move;
+        if (u % 2 == 0) {
+            move = 'X';
+            return move;
+        }
+        else if (u % 2 != 0) {
+            move = 'O';
+            return move;
+        }
+    }
+    return turn;
 
 }
-let box = document.querySelectorAll('div');
-console.log(box)
+
 
 //There should be an object to control the flow of the game itself
 
 //Creation of the gameboard. Possibly to be put into Gameboard() object
 const cells = (() => {
-    let grid = document.querySelector('#grid');
-    for (let i = 0; i < 9; i++) {
-        let content = document.createElement('button');
-        content.classList.add('box')
-        content.classList.add(i);
-        const testing = Object.values(Gameboard());
-        const test2 = testing[0][i][i];
-        content.textContent = test2
-        grid.appendChild(content);
-    };
+    let grid = document.querySelectorAll('.grid-row');
+    grid.forEach(div => {
+        for (let i = 0; i < 3; i++) {
+            let content = document.createElement('button');
+            content.classList.add('box');
+            content.classList.add(i)
+            const testing = Object.values(Gameboard());
+            const test2 = testing[0][i][i];
+            content.textContent = test2
+            div.appendChild(content);
+        };
+    })
+
 })();
+let box = document.querySelectorAll('button');
+box.forEach((button) => {
+    button.addEventListener('click', function () {
+        button.textContent = 'X';
+    })
+})
