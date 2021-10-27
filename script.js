ee8a87acb2cef1d88d705f1a054c796a06bac924
Duvauwn Prendergast <duvauwnprendergast@gmail.com>
2021-10-27 22:38:29 +0100

    updating gameboard based on player movement

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
 //The Gameboard will be stored as an array inside of a Gameboard object
-function Gameboard() {
+const Gameboard = (() => {
     let board0 = { 0: '1', 1: '2', 2: '3', };
     let board1 = { 0: '4', 1: '5', 2: '6', };
     let board2 = { 0: '7', 1: '8', 2: '9', };
-    console.log('testing');
-    return [Gameboard || { board0, board1, board2 }];
-}
+
+    //Creation of the gameboard. Possibly to be put into Gameboard() object
+    const cells = (() => {
+        let grid = document.querySelectorAll('.grid-row');
+        grid.forEach(div => {
+            for (let i = 0; i < 3; i++) {
+                let content = document.createElement('button');
+                content.classList.add(i);
+                const boards = [board0, board1, board2];
+                if (div.className == 'grid-row a') {
+                    if (content.className == 0) {
+                        content.textContent = boards[0][0];
+                    }
+                    else if (content.className == 1) {
+                        content.textContent = boards[0][1];
+                    }
+                    else if (content.className == 2) {
+                        content.textContent = boards[0][2];
+                    }
+                }
+                else if (div.className == 'grid-row b') {
+                    if (content.className == 0) {
+                        content.textContent = boards[1][0];
+                    }
+                    else if (content.className == 1) {
+                        content.textContent = boards[1][1];
+                    }
+                    else if (content.className == 2) {
+                        content.textContent = boards[1][2];
+                    }
+                }
+                else if (div.className == 'grid-row c') {
+                    if (content.className == 0) {
+                        content.textContent = boards[2][0];
+                    }
+                    else if (content.className == 1) {
+                        content.textContent = boards[2][1];
+                    }
+                    else if (content.className == 2) {
+                        content.textContent = boards[2][2];
+                    }
+                }
+                div.appendChild(content);
+            };
+        })
+
+    })();
+
+    return { board0, board1, board2, cells };
+})();
 //The players will also be stored inside objects
 let u = 0;
 const Player = () => {
@@ -39,52 +86,7 @@ const Player = () => {
 //There should be an object to control the flow of the game itself
 
 
-//Creation of the gameboard. Possibly to be put into Gameboard() object
-const cells = (() => {
-    let grid = document.querySelectorAll('.grid-row');
-    grid.forEach(div => {
-        for (let i = 0; i < 3; i++) {
-            let content = document.createElement('button');
-            content.classList.add(i);
-            const boards = Object.values(Gameboard());
-            if (div.className == 'grid-row a') {
-                if (content.className == 0) {
-                    content.textContent = boards[0][0];
-                }
-                else if (content.className == 1) {
-                    content.textContent = boards[0][1];
-                }
-                else if (content.className == 2) {
-                    content.textContent = boards[0][2];
-                }
-            }
-            else if (div.className == 'grid-row b') {
-                if (content.className == 0) {
-                    content.textContent = boards[1][0];
-                }
-                else if (content.className == 1) {
-                    content.textContent = boards[1][1];
-                }
-                else if (content.className == 2) {
-                    content.textContent = boards[1][2];
-                }
-            }
-            else if (div.className == 'grid-row c') {
-                if (content.className == 0) {
-                    content.textContent = boards[2][0];
-                }
-                else if (content.className == 1) {
-                    content.textContent = boards[2][1];
-                }
-                else if (content.className == 2) {
-                    content.textContent = boards[2][2];
-                }
-            }
-            div.appendChild(content);
-        };
-    })
 
-})();
 let box = document.querySelectorAll('button');
 box.forEach((button) => {
     let gridA = document.querySelector('.a');
@@ -96,35 +98,35 @@ box.forEach((button) => {
 
         if (button.parentElement == gridA) {
             if (button.className == 0) {
-                console.log('A: ' + button.className);
+                Gameboard.board0[0] = button.textContent;
             }
             else if (button.className == 1) {
-                console.log('A: ' + button.className);
+                Gameboard.board0[1] = button.textContent;
             }
             else if (button.className == 2) {
-                console.log('A: ' + button.className);
+                Gameboard.board0[2] = button.textContent;
             }
         }
         else if (button.parentElement == gridB) {
             if (button.className == 0) {
-                console.log('B: ' + button.className);
+                Gameboard.board1[0] = button.textContent;
             }
             else if (button.className == 1) {
-                console.log('B: ' + button.className);
+                Gameboard.board1[1] = button.textContent;
             }
             else if (button.className == 2) {
-                console.log('B: ' + button.className);
+                Gameboard.board1[2] = button.textContent;
             }
         }
         else if (button.parentElement == gridC) {
             if (button.className == 0) {
-                console.log('C: ' + button.className);
+                Gameboard.board2[0] = button.textContent;
             }
             else if (button.className == 1) {
-                console.log('C: ' + button.className);
+                Gameboard.board2[1] = button.textContent;
             }
             else if (button.className == 2) {
-                console.log('C: ' + button.className);
+                Gameboard.board2[2] = button.textContent;
             }
         }
         button.disabled = true;
